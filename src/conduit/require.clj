1a9ed801080b86c391bab3679191c11004b4289c
Jim Duey <jim@intensivesystems.net>
2010-11-14 10:31:54 -0600

    Changed some names in require namespace

--- src/conduit/require.clj
+++ src/conduit/require.clj
@@ -3,10 +3,16 @@
   (:refer-clojure :exclude [case comp juxt reduce map]))
 
 (def proc a-arr)
+(def nth a-nth)
 (def case a-selectp)
-(def comp a-comp)
+(def pipe a-comp)
 (def split a-all)
 (def juxt a-par)
 (def reduce a-loop)
 (def map conduit-map)
 (def do conduit-do)
+(defmacro def* [name args & body]
+  `(def ~name (conduit-proc (fn ~name ~args ~@body))))
+(defmacro def [name args & body]
+  `(def ~name (a-arr (fn ~name ~args ~@body))))
+
